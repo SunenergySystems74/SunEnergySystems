7bb5773051f5bbc7e4eb771b41abbe328b216ae9
Shyam Nair <110102576+Shyamnair488@users.noreply.github.com>
2025-01-17 20:56:28 +0530

    Description of your changes

--- sunenergy/src/Screens/Contact.js
+++ sunenergy/src/Screens/Contact.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import axios from "axios";
+import emailjs from "emailjs-com";
+import React, { useRef, useState } from "react";
 import "./Contact.css";
 
 const Contact = () => {
@@ -9,28 +9,39 @@ const Contact = () => {
     phone: "",
     message: "",
   });
-
-  const [responseMessage, setResponseMessage] = useState("");
+  const form = useRef();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    axios
-      .post(
-        "https://sunenergysystem-lbihyevv5-sunenergysystems74s-projects.vercel.app/send-email",
-        formData
+    emailjs
+      .sendForm(
+        "service_aynz5tk", // Your EmailJS service ID
+        "template_lusdpgl", // Your EmailJS template ID
+        form.current,
+        "bOhvfenfxAL6enWMm" // Your EmailJS user ID
       )
-      .then((response) => {
-        setResponseMessage("Message sent successfully!");
-      })
-      .catch((error) => {
-        setResponseMessage("Failed to send the message. Please try again.");
-      });
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message sent successfully!");
+          setFormData({
+            name: "",
+            email: "",
+            phone: "",
+            message: "",
+          }); // Clear form fields
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Message sending failed, please try again.");
+        }
+      );
   };
 
   return (
@@ -43,7 +54,7 @@ const Contact = () => {
       <div className="contact-content">
         <div className="contact-form">
           <h2>Get In Touch</h2>
-          <form onSubmit={handleSubmit}>
+          <form ref={form} onSubmit={sendEmail}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -90,20 +101,16 @@ const Contact = () => {
             </div>
             <button type="submit">Submit</button>
           </form>
-          <p>{responseMessage}</p>
         </div>
 
-        {/* Contact Information */}
         <div className="contact-info">
           <h2>Our Office</h2>
           <p>
-            MIDC Hingna road,Near Electronic Zone Square, Nagpur, Maharashtra
-            440016
+            MIDC Hingna road, Near Electronic Zone Square, Nagpur, Maharashtra 440016
           </p>
           <p>Email: sunenergysystems74@gmail.com</p>
           <p>Phone: +91 9673188352</p>
 
-          {/* Location Map */}
           <div className="map-container">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722.07008419544!2d78.97771217471656!3d21.109771884992224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd49538ae8a5a7d%3A0xe5be2b89cf294ca3!2sSun%20Energy%20systems!5e0!3m2!1sen!2sin!4v1727105099768!5m2!1sen!2sin"
